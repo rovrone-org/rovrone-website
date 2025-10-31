8242c02b15cc007dfd37b3c8142a646f2c7180f5
sage-rebirth <abhinavs1711@gmail.com>
2025-10-31 21:36:56 +0530

    app modified for vercel deployment

--- server.js
+++ server.js
@@ -12,6 +12,22 @@ app.use(express.json());
 
 app.use('/api', apiRouter);
 
+// Serve frontend
+const frontendPath = path.resolve(__dirname, 'frontend', 'dist');
+app.use(express.static(frontendPath));
+
+app.use((req, res, next) => {
+  if (req.method === 'GET' && !req.path.startsWith('/api')) {
+    res.sendFile(path.resolve(frontendPath, 'index.html'), (err) => {
+      if (err) {
+        res.status(500).send(err);
+      }
+    });
+  } else {
+    next();
+  }
+});
+
 app.get('/health', (req, res) => {
   res.json({ success: true, data: { status: 'ok' } });
 });
